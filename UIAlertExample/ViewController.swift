16dfec2466d282890cc3a57fab017d5ddb1cd3d7
Sean Goldsborough <smgoldsborough@gmail.com>
2018-07-17 17:07:47 -0400

    refactor: removes unnecessary print statements

--- UIAlertExample/ViewController.swift
+++ UIAlertExample/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Application did finish launching")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
